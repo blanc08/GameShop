f966be869d83b19c7c654f9dc88ae29f3f11fbc0
bugss02 <bagusoktaviadi1@gmail.com>
2021-10-06 15:23:08 +0700

    some update on GameDetail

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -1,16 +1,31 @@
 import { useRouter } from 'next/router'
-import { useEffect } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import TopUpItem from '../../components/molecules/TopUpItem'
 import Footer from '../../components/organisms/Footer'
 import Navbar from '../../components/organisms/Navbar'
 import TopUpForm from '../../components/organisms/TopUpForm'
+import { getDetailVoucher } from '../../services/player'
 
 export default function Detail() {
   const { query, isReady } = useRouter()
+  const [dataItem, setDataItem] = useState({
+    name: '',
+    thumbnail: '',
+    category: {
+      name: '',
+    },
+  })
+
+  const getVoucherDetailAPI = useCallback(async (id) => {
+    const data = await getDetailVoucher(id)
+    console.log('data :', data)
+    setDataItem(data)
+  }, [])
 
   useEffect(() => {
     if (isReady) {
       console.log('router sudah tesedia', query.id)
+      getVoucherDetailAPI(query.id)
     } else {
       console.log('blum tersedia')
     }
@@ -30,10 +45,10 @@ export default function Detail() {
           </div>
           <div className="row">
             <div className="col-xl-3 col-lg-4 col-md-5 pb-30 pb-md-0 pe-md-25 text-md-start">
-              <TopUpItem isDesktop />
+              <TopUpItem data={dataItem} isDesktop />
             </div>
             <div className="col-xl-9 col-lg-8 col-md-7 ps-md-25">
-              <TopUpItem isMobile />
+              <TopUpItem data={dataItem} isMobile />
 
               <hr />
               <TopUpForm />
